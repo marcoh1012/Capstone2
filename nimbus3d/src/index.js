248416005793c5ebce6622b15caecc90d93196a8
Marco Herrera <marcoh1012@gmail.com>
2020-12-27 21:19:33 -0600

    persistedStore

--- nimbus3d/src/index.js
+++ nimbus3d/src/index.js
@@ -4,16 +4,15 @@ import './index.css';
 import App from './App';
 import reportWebVitals from './reportWebVitals';
 
-import { Provider } from 'react-redux';
-import { createStore } from 'redux'
-import rootReducer from './rootReducer';
+import { PersistGate } from "redux-persist/integration/react";
+import { Provider } from "react-redux";
+import { store, persistedStore} from "./store"
 
-const store = createStore(rootReducer)
 ReactDOM.render(
   <Provider store={store}>
-  <React.StrictMode>
-    <App />
-  </React.StrictMode>
+    <PersistGate loading={null} persistor={persistedStore}>
+      <App/>
+    </PersistGate>
   </Provider>,
   document.getElementById('root')
 );
---
+++ nimbus3d/src/store.js
@@ -0,0 +1,24 @@
+import  { composeWithDevTools} from "redux-devtools-extension";
+import { persistStore, persistReducer } from "redux-persist";
+import storage from 'redux-persist/lib/storage'
+
+import thunk from "redux-thunk";
+import rootReducer from "./rootReducer";
+import { createStore, applyMiddleware } from "redux";
+
+const persistConfig = {
+  key: "root",
+  storage
+};
+
+const persistedReducer = persistReducer(persistConfig, rootReducer);
+
+
+export const store = createStore(
+  persistedReducer,
+  composeWithDevTools(
+    applyMiddleware(thunk),
+  )
+);
+
+export const persistedStore = persistStore(store);
